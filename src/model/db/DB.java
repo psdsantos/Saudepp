70fe60dae07e5f15e6a06a04e40b6726cc2765c9
sdsPedro <pedro.santos061@academico.ifs.edu.br>
2019-10-09 17:12:21 -0300

    Add DB.java, db.properties

--- src/model/db/DB.java
+++ src/model/db/DB.java
@@ -1,5 +1,82 @@
 package model.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Properties;
+
 public class DB {
-	
+	private static Connection connection = null;
+
+	public static Connection getConnection() {
+		if (connection == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				connection = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+
+	public static void closeConnection() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static Properties loadProperties() {
+		Properties props = new Properties();
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			props.load(fs);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return props;
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void incluir(String tabela, List<String> valores) throws SQLException {
+		String sql;
+		Statement statement = getConnection().createStatement();
+		sql = String.format("insert into %s values (", tabela);
+
+		for (String valor : valores) {
+			sql += "'" + valor + "',";
+		}
+		sql = sql.substring(0, sql.length()-1) + ")";
+
+		statement.executeUpdate(sql);
+		statement.close();
+	}
 }
