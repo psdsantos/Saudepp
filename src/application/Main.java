563912e7c8847a0ba5448a213ac845f68c54834b
sdsPedro <pedro.santos061@academico.ifs.edu.br>
2019-11-04 14:22:21 -0300

    Handle invalid db.properties

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,18 @@
 package application;
 
 import java.nio.file.Paths;
+import java.sql.SQLException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import model.db.DB;
+import model.exceptions.InvalidDBPropertiesException;
 import model.util.WindowsParam;
 
 public class Main extends Application {
@@ -31,8 +35,14 @@ public class Main extends Application {
 			primaryStage.show();
 			
 			DB.closeConnection();
-			DB.getConnection();
+			if(DB.getConnection() == null) throw new InvalidDBPropertiesException();
 			DB.changeDateStyle("ISO, DMY");
+		} catch (InvalidDBPropertiesException e) {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("AVISO");
+			alert.setHeaderText("Propriedades do banco de dados inválidas");
+			alert.setContentText("Verifique o arquivo de propriedades.");
+			alert.showAndWait();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/model/exceptions/InvalidDBPropertiesException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class InvalidDBPropertiesException extends Exception{
+	
+	private static final long serialVersionUID = 2L;
+	
+	public InvalidDBPropertiesException() {
+		super();
+	}
+}
